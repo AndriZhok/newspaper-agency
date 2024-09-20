fe6f004a89345655994cc4d89dac5f65f00d7e8a
Andriy Zhuk <andri195595@gmail.com>
2024-09-20 20:22:41 +0300

    add new allowed host

--- newspaperAgency/settings.py
+++ newspaperAgency/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
 
-ALLOWED_HOSTS = ["localhost"]
+ALLOWED_HOSTS = ["localhost", "newspaper-agency-rele.onrender.com"]
 
 
 # Application definition
